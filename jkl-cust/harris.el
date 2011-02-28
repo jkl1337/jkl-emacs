0c210ae79d6c650360618ceaa16060fd0c47d9fa
John K. Luebs <jkluebs@luebsphoto.com>
2011-02-28 18:03:56 -0500

    Broke out red and black side setup functions

--- jkl-cust/harris.el
+++ jkl-cust/harris.el
@@ -16,27 +16,33 @@
   (interactive))
 
 ; This just serves as notes basically, need to clean this up.
-(defun hft/do-stuff ()
+(defun hft/do-black ()
+  (interactive)
   (copy-face 'default 'hft-black-face)
-  (copy-face 'default 'hft-red-face)
   (set-face-attribute 'hft-black-face nil
 		      :background "#000000"
 		      :foreground "#fffff0")
+  (serial-term hft/black-port 115200)
+
+  (with-current-buffer hft/black-port
+    (buffer-face-set 'hft-red-face)
+    (set (make-local-variable 'emulation-mode-map-alists) nil)
+    (set (make-local-variable 'term-default-bg-color) "#000000")
+    (set (make-local-variable 'term-default-fg-color) "#fffff0")
+    (term-ansi-reset)))
+
+(defun hft/do-red ()
+  (interactive)
+  (copy-face 'default 'hft-red-face)
   (set-face-attribute 'hft-red-face nil
 		      :background "#640000"
 		      :foreground "#fffff0")
 
   (serial-term hft/red-port 115200)
-  (serial-term hft/black-port 115200)
 
   (with-current-buffer hft/red-port
+    (buffer-face-set 'hft-red-face)
     (set (make-local-variable 'emulation-mode-map-alists) nil)
     (set (make-local-variable 'term-default-bg-color) "#640000")
-    (set (make-local-variable 'term-default-fg-color) "#fffff0")
-    (term-ansi-reset))
-
-  (with-current-buffer hft/black-port
-    (set (make-local-variable 'emulation-mode-map-alists) nil)
-    (set (make-local-variable 'term-default-bg-color) "#000000")
     (set (make-local-variable 'term-default-fg-color) "#fffff0")
     (term-ansi-reset)))
