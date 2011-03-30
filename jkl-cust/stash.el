5ba26447c0d11c1bb7024f979b0ad2a388bf7ecf
John K. Luebs <jkluebs@luebsphoto.com>
2011-03-30 18:45:33 -0400

    Stash/scratch file added

---
+++ jkl-cust/stash.el
@@ -0,0 +1,12 @@
+(when jkl/mswinp
+  (setq pylookup-program (concat pylookup-dir "pylookup.bat"))
+  (unless (file-exists-p pylookup-program)
+    (let ((explicit-shell-file-name "cmd.exe"))
+      (with-temp-buffer
+	(write-region
+	 (concat "@python "
+		 (convert-standard-filename
+		  (concat pylookup-dir "pylookup.py")) " %*")
+	 nil pylookup-program)))
+    t))
+
