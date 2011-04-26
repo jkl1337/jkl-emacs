83b77ca1c2abdff1a991f244084af7757171287b
John K. Luebs <jkluebs@luebsphoto.com>
2011-04-26 18:14:09 -0400

    Remove ilisp nonsense and use w3m for online lisp docs

--- jkl-cust/clisp-setup.el
+++ jkl-cust/clisp-setup.el
@@ -17,43 +17,4 @@
      `(,(concat "clisp"
                 (if (file-directory-p jkl/clisp-dir)
                     " -K full" ""))
-       "c:/prp/ccl/wx86cl.exe"))
-
-(jkl/custom-set 'inferior-lisp-program (car jkl/slime-lisps))
-
-;;; ILISP/Inferior Lisp hook customizations
-(defun jkl/setup-ilisp ()
-  "Set up common variables used by ilisp."
-  (interactive)
-  (setq ilisp-*use-fsf-compliant-keybindings* t
-        ilisp-*arglist-message-lisp-space-p* t
-        ilisp-print-info-message-command t
-        lisp-no-popper t)
-
-  (add-hook 'lisp-mode-hook
-            (lambda ()
-              (imenu-add-to-menubar "Symbols")
-              (outline-minor-mode)
-              (make-local-variable 'outline-regexp)
-              (setq outline-regexp "^(.*")
-              (ignore-errors (semantic-default-elisp-setup))
-              (set (make-local-variable lisp-indent-function)
-                   'common-lisp-indent-function)))
-
-  (add-hook 'ilisp-load-hook
-            '(lambda ()
-               (defkey-ilisp [(control c) (e)] 'eval-in-lisp)
-               (defkey-ilisp [(control c) (\;)] 'insert-balanced-comments)
-               (defkey-ilisp [(control c) (:)] 'remove-balanced-comments)
-               (defkey-ilisp [(control c) (x)] 'copy-eval-dwim-lisp)
-               ;; Set the inferior Lisp directory to the directory of
-               ;; the buffer that spawned it on the first prompt.
-               (add-hook 'ilisp-init-hook
-                         '(lambda ()
-                            (default-directory-lisp ilisp-last-buffer)))))
-  (require 'completer)
-  (require 'ilisp)
-  ;; Fix clisp interaction buffer (Windows)
-  ;; (modify-coding-system-alist 'process "lisp" 'unix)
-  ;; All the *.d and *.lisp sources are in UTF-8 encoding.
-  (modify-coding-system-alist 'file "\\.\\(d\\|lisp\\)\\'" 'utf-8))
+       "c:/prp/ccl-trunk/wx86cl.exe"))
