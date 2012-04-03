65e2cd39b80f345ebd9cbcdcee3b8a2b9f0f4c0e
John K. Luebs <jkluebs@luebsphoto.com>
2012-04-02 22:48:19 -0400

    Fix stupid typo for python-mode

--- init.el
+++ init.el
@@ -146,7 +146,7 @@
                             "Python editing mode." t)
                           (add-to-list 'auto-mode-alist
                                        '("\\.py$" . python-mode))
-                          (add-to-list 'interpreter-mode-list
+                          (add-to-list 'interpreter-mode-alist
                                        '("python" . python-mode))))
 
         ;; stupid org-mode git broke
