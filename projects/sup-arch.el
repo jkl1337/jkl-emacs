372c9b721c225ca3c9153776dc4fa6667ab2db29
John K. Luebs <jkluebs@luebsphoto.com>
2012-09-05 16:41:11 -0400

    Add sup project

---
+++ projects/sup-arch.el
@@ -0,0 +1,5 @@
+(ede-cpp-root-project "sup-arch"
+                      :name "sup-arch"
+                      :file "~/git/sup-arch/sup/Makefile"
+                      :include-path '("/")
+                      :system-include-path '("/usr/include/c++/4.7.1"))
