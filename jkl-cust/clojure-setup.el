91cbb25f1e5eee880511d8e2175d50d8279ce271
John K. Luebs <john.luebs@accunurse.com>
2012-12-14 21:38:01 -0500

    Remove obsolete nrepl interaction mode hook

--- jkl-cust/clojure-setup.el
+++ jkl-cust/clojure-setup.el
@@ -3,15 +3,10 @@
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes 'nrepl-mode))
 
-(add-hook 'nrepl-interaction-mode-hook
-          (lambda ()
-            (ac-nrepl-setup)
-            (nrepl-turn-on-eldoc-mode)
-            (paredit-mode)))
-
 (add-hook 'nrepl-mode-hook
           (lambda ()
             (subword-mode)
             (ac-nrepl-setup)
+            (nrepl-turn-on-eldoc-mode)
             (paredit-mode)))
 
