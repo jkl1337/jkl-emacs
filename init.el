9ee5e3f934a1d382a0b1e3a27947eb06b5fbb4a8
John K. Luebs <jkluebs@luebsphoto.com>
2013-04-14 15:28:31 -0400

    Add convenience function to open files with sudo directly

--- init.el
+++ init.el
@@ -433,6 +433,40 @@ try disabling Alt-Tab switching and see how that works")
 (jkl/add-to-list 'tramp-default-proxies-alist '(nil "\\`root\\'" "/ssh:luebsj@%h:"))
 (jkl/add-to-list 'tramp-default-proxies-alist '((regexp-quote (system-name)) nil nil))
 
+(eval-after-load "tramp"
+  '(progn
+     (defvar sudo-tramp-prefix
+       "/sudo:"
+       (concat "Prefix to be used by sudo commands when building tramp path "))
+
+     (defun sudo-file-name (filename)
+       (let* ((splitname (split-string filename ":"))
+              (components (or (cdr splitname)
+                              (cons (concat "root@" (system-name)) splitname))))
+         (concat sudo-tramp-prefix (mapconcat 'identity components ":"))))
+
+     (defun sudo-find-file (filename &optional wildcards)
+       "Calls find-file with filename with sudo-tramp-prefix prepended"
+       (interactive "fFind file with sudo ")
+       (let ((sudo-name (sudo-file-name filename)))
+         (apply 'find-file
+                (cons sudo-name (if (boundp 'wildcards) '(wildcards))))))
+
+     (defun sudo-reopen-file ()
+       "Reopen file as root by prefixing its name with sudo-tramp-prefix and by clearing buffer-read-only"
+       (interactive)
+       (let*
+           ((file-name (expand-file-name buffer-file-name))
+            (sudo-name (sudo-file-name file-name)))
+         (progn
+           (setq buffer-file-name sudo-name)
+           (rename-buffer sudo-name)
+           (setq buffer-read-only nil)
+           (message (concat "File name set to " sudo-name)))))
+
+     (global-set-key "\C-x+" 'sudo-find-file)
+     (global-set-key "\C-x!" 'sudo-reopen-file)))
+
 ;;; GLOBAL settings
 (find-function-setup-keys)
 (define-key 'help-command "\C-f" 'find-function)
